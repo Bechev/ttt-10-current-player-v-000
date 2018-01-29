7bb0d0553a45b2c0ec46b9698a5b3a19f669c948
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-29 12:09:28 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -16,6 +16,6 @@ def current_player(board)
   #else
     #return "O"
   #end
-
-  number_of_turns = (turn_count(board) % 2) ? "X": "O"
+  player = (turn_count(board) % 2) ? "X": "O"
+  return player
 end
